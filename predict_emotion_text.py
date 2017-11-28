8e4e24a63ca21dcf4675e31255320a1487baeb7b
light0617 <light11470617@gmail.com>
2017-11-28 09:39:22 +0000

    java

--- predict_emotion_text.py
+++ predict_emotion_text.py
@@ -1,5 +1,6 @@
 import json
 from nltk import corpus
+import nltk
 from nltk.corpus import wordnet as wn
 import numpy as np
 from lib import *
@@ -11,21 +12,40 @@ LEXICON = 'lexicon/Semeval_2007/lexicon.json'
 def predict_sentence(wp, classifiers, testSetence, emotions):
 	return [test_line(wp, classifiers[emo], testSetence, emo) for emo in emotions]
 
-def predict_text(text, lexicon, wp, classifiers):
+def predict_text(text, lexicon = None, wp = None, classifiers = None):
+    if not lexicon:
+        lexicon = load_json(LEXICON)
+    if not wp or not classifiers:
+        ml_model = 'logistic'
+        path = 'pickled'
+        domain = 'Semeval_2007'
+        model = 'model1'
+        classifiers = {}
+        for emotion in ['joy', 'sad', 'disgust', 'anger', 'surprise', 'fear']:
+            classifiers[emotion], wp = predict_init(path, domain, model, emotion, ml_model)
+        
 	text = text.split(". ")
 	num_sentence = len(text)
 	emotions_score = [[] for _ in range(6)]
 	for sentence in text:
 		total, none_word_in_lexicon = 0, True
 		for w in sentence.split(' '):
-			w = wn.morphy(w.strip('\.\n'))
+			try:
+				 w = wn.morphy(w.strip('\.\n'))
+			except:
+				nltk.download('wordnet')
 			if w in lexicon:
 				total = sum([float(lexicon[w][emotion]) for emotion in lexicon[w]])
 				if total > 0: 
 					none_word_in_lexicon = False
 					break
 		if not none_word_in_lexicon:
-			score = predict_sentence(wp, classifiers, sentence, EMOTIONS)
+			try:
+				score = predict_sentence(wp, classifiers, sentence, EMOTIONS)
+			except:
+				nltk.download('stopwords')
+				nltk.download('punkt')
+				score = predict_sentence(wp, classifiers, sentence, EMOTIONS)
 			for i in range(len(EMOTIONS)):
 				if len(emotions_score[i]) > 2 and \
 					len(emotions_score[i]) >= num_sentence // 2:
